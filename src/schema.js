06ccf1e6d591566964736570422f1f016f8b32da
dessant <git@rmn.space>
2019-01-20 07:24:42 +0200

    fix: apply stricter config validation

--- src/schema.js
+++ src/schema.js
@@ -3,7 +3,11 @@ const Joi = require('joi');
 const schema = Joi.object().keys({
   openIssueColumns: Joi.array()
     .single()
-    .items(Joi.string())
+    .items(
+      Joi.string()
+        .trim()
+        .max(140)
+    )
     .default([])
     .description(
       'Open issues that are moved to these project columns. Set to `[]` to disable'
@@ -11,13 +15,20 @@ const schema = Joi.object().keys({
 
   closedIssueColumns: Joi.array()
     .single()
-    .items(Joi.string())
+    .items(
+      Joi.string()
+        .trim()
+        .max(140)
+    )
     .default(['Closed', 'Done'])
     .description(
       'Close issues that are moved to these project columns. Set to `[]` to disable'
     ),
 
-  _extends: Joi.string().description('Repository to extend settings from'),
+  _extends: Joi.string()
+    .trim()
+    .max(260)
+    .description('Repository to extend settings from'),
 
   perform: Joi.boolean().default(!process.env.DRY_RUN)
 });
